509301ad1af9cfb575a5dfdd4c1d1faad9dd04fb
Susan Marek <smarek@chaskamn.com>
2018-03-16 07:44:48 -0500

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,17 +3,22 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var db = require("../models/burgertable.js");
+var db = require("../models");
 
 // Update for var burgerAction
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  console.log(db);
+  // console.log(db);
   db.Burgertable.findAll({}).then(function(results){
-
-    res.render("index", json(results));
+  	var hbsObject = {
+  		burgers: results
+  	};
+  	console.log("XXXXXXXXXXXX");
+  	console.log(hbsObject);
+  	res.render("index", hbsObject);
   });
 });
+
 
 // router.post("/api/burgers", function(req, res) {
 //   burgerAction.insertOne([
@@ -25,6 +30,30 @@ router.get("/", function(req, res) {
 //     res.json({ id: result.insertId });
 //   });
 // });
+
+// router.post("/api/burgers", function(req, res) {
+//   burgerAction.insertOne([
+//     "burger_name", "devoured"
+//   ], [
+//     req.body.burger_name, false
+//   ], function(result) {
+//     // Send back the ID of the burger
+//     res.json({ id: result.insertId });
+//   });
+// });
+
+  router.post("/api/burgers", function(req, res) {
+    console.log("New burger:-----------------------");
+    console.log(req.body);
+    db.Burgertable.create({
+      burger_name: req.body.burger_name,
+      devoured: false,
+    }).then (function(result) {
+      // We have access to the new todo as an argument inside of the callback function
+      res.json({ id: result.insertId });
+    });
+   
+  });
 
 // router.put("/api/burgers/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
@@ -42,6 +71,24 @@ router.get("/", function(req, res) {
 //     }
 //   });
 // });
+
+
+  app.put("/api/burgers/:id", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    // var condition = "id = " + req.params.id;
+    db.Burgertable.update({
+      devoured: req.body.devoured,
+    }, {
+      where: {
+        id: req.params.id
+      }
+    // }).then(function(dbTodo) {
+    //   res.json(dbTodo);
+    });
+  });
+
+};
 
 
 // Export routes for server.js to use.
